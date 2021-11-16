3d137467c9e23da7ca51c40cb4ba1b36d4cf0d67
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-16 18:14:42 -0500

    implementing composer char counter to count and change colors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,8 @@
 $(document).ready(()=>{
-  console.log('ready');
+  $("#tweet-text").on('input', function() {
+    const $counter = $(this).parent().siblings().find('.counter');
+    const counter = 140 - $(this).val().length;
+    counter < 0 ? $counter.addClass('text-red') : $counter.removeClass('text-red');
+    $counter.text(counter);
+  });
 });

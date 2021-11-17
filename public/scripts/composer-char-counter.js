c097c1d7dae22e9bcf52c641532b47cb56dada57
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-16 20:27:34 -0500

    adding simple comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,13 @@
 $(document).ready(()=>{
+  // Listen for input events on #tweet-text and run callback accordingly
   $("#tweet-text").on('input', function() {
+    // Saving JQuery target for counter class in $counter
     const $counter = $(this).parent().siblings().find('.counter');
+    // Computing the new value of counter
     const counter = 140 - $(this).val().length;
+    // Change counter color to red if below 0
     counter < 0 ? $counter.addClass('text-red') : $counter.removeClass('text-red');
+    // Update counter value on each input event
     $counter.text(counter);
   });
 });

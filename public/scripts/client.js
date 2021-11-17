089b6a0663ef71f525257bf5d66c33d06bccb643
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-17 16:48:32 -0500

    added dynamic view of tweets through scripts (hardcoded db)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,25 +30,40 @@ $(document).ready(() => {
   };
 
   const renderTweets = (tweetsArr) => {
-    const $tweet = tweet(tweetData);
-    $('#tweets-area').append($tweet);
-    console.log('Tweeted!', $tweet);
+    for (const tweetObj of tweetsArr) {
+      const $tweet = tweet(tweetObj);
+      $('#tweets-area').append($tweet);
+      console.log('Tweeted!', $tweet);
+    }
+    
   };
   
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-  
-  
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
   // Test / driver code (temporary)
-  // to see what it looks like
+  renderTweets(data);
 });

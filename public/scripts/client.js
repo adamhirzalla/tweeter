27b85e10d743ba667ffd2f5325fb55667725233a
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-17 18:09:29 -0500

    adding ajax request to fetch data from initial db and callback rendertweets fn

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -50,45 +50,15 @@ $(document).ready(() => {
     }
     
   };
+
+  const loadTweets = () => {
+    $.ajax('/tweets/', { method: 'GET' })
+      .then(function(data) {
+        console.log('Success: ', data);
+        renderTweets(data);
+      });
+  };
   
   // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Adam",
-        "avatars": "/images/avatar.png"
-        ,
-        "handle": "@AdamH"
-      },
-      "content": {
-        "text": "I learn from the mistakes of people who took my advice"
-      },
-      "created_at": new Date()
-    },
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-  // Test / driver code (temporary)
-  renderTweets(data);
+  loadTweets();
 });
